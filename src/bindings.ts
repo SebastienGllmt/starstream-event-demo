fa015e1377be565e5ea6f8001250085e5cc8f1a9
Sebastien Guillemot <SebastienGllmt@gmail.com>
2026-04-14 08:31:54 +0100

    add satifies checks

--- src/bindings.ts
+++ src/bindings.ts
@@ -1,5 +1,8 @@
-export let blockHeightValue: number = 7000;
-export function setBlockHeight(value: number) {
+type CardanoChain = typeof import ("../gen-wasm/component.wasm").StarstreamStdCardano;
+type ContractAbi = typeof import ("../gen-wasm/component.wasm").MyAbi;
+
+export let blockHeightValue: bigint = 7000n;
+export function setBlockHeight(value: bigint) {
   blockHeightValue = value;
 }
 
@@ -9,14 +12,14 @@ const bindings = {
     action(blockHeight: bigint): void {
       console.log("action called at:", blockHeight);
     }
-  },
+  } satisfies ContractAbi,
 
   // in theory, this would be handled by the node
   "starstream:std/cardano": {
     blockHeight() {
       return blockHeightValue;
     },
-  }
+  } satisfies CardanoChain
 };
 
 export const action = bindings["my-abi"].action;
