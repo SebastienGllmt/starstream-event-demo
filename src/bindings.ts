b911651d5e738c1793b47fbbca5f84e8fbca34d3
Sebastien Guillemot <SebastienGllmt@gmail.com>
2026-04-14 07:49:53 +0100

    Add comments

--- src/bindings.ts
+++ src/bindings.ts
@@ -4,12 +4,14 @@ export function setBlockHeight(value: number) {
 }
 
 const bindings = {
+  // handled by the user to catch effects from the contract (like events)
   "my-abi": {
     action(blockHeight: bigint): void {
       console.log("action called at:", blockHeight);
     }
   },
 
+  // in theory, this would be handled by the node
   "starstream:std/cardano": {
     blockHeight() {
       return blockHeightValue;
